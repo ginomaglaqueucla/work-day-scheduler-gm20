0dbc0eb23d960a04c2c9508011d1e969c2721312
ginomaglaque <ginomaglaque@gmail.com>
2020-09-06 15:28:56 -0700

    Made comments and cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var schedule = {};
 var currentDate;
+
+// Load schedule onto page from local storage
 var loadEvent = function() {
-    console.log("Hello");
     // pull local storage schedule
     schedule = JSON.parse(localStorage.getItem("schedule"));
 
@@ -14,11 +15,14 @@ var loadEvent = function() {
         };
     } 
     
+    // loop through each item in object array in local storage
+    // pass to createEvent function
     for( var i = 0; i < schedule.hour.length; i++){
         createEvent(schedule.hour[i], schedule.eventMsg[i]);
     }   
 };
 
+// pulls current date data using moment()
 var loadDay = function() {
     currentDate = moment();
     var currentDay = moment().format("dddd, MMMM Do YYYY");
@@ -28,72 +32,75 @@ var loadDay = function() {
 
 // displays events respectively
 var createEvent = function(hour, message){
-    console.log("inside createEvent");
-    // var textArea = $("<textarea>")
-    //     .text(message);
-    
+    // select HTML element for event message/description    
     var eventMsg = $("#"+hour+"-event");
 
+    // append message to display onto page
     eventMsg.append(message);
 };
 
 
-// when an event box is clicked
+// when an event description box is clicked
 $(".row").on("click", "textarea", function(){
-    console.log("Inside event meesage");
-    
+    // capture newly inputted text    
     var text = $(this)
         .text()
         .trim();
 
+    // reassign
     $(this).text(text);
 
 });
 
 // when action happens outside of being inside the text area
+// in order for event box to retain value
 $(".row").on("blur", "textarea", function() {
-    console.log("inside blur");
+    // grab newly inputted text
     var text = $(this)
         .val()
         .trim();
     
+    // select HTML parent element
     var rowID = $(this)
         .closest(".row")
         .attr("id");
     
+    // index of where user just inputted from
     var index = schedule.hour.indexOf(rowID);
-    console.log(text);
     
+    // reassign in object arrat
     schedule.eventMsg[index] = text;
 });
 
+// when save button is clicked
 $(".row").on("click", "button", function(){
-    console.log("save");
+    // reassign schedule object inside local storage
     localStorage.setItem("schedule", JSON.stringify(schedule));
 });
 
+// checks timeblocks according to current hour
 var checkDue = function(){
+    // pull latest date data
     loadDay();
+    // parse hour to integer
     var currentHour = parseInt(currentDate.format("H"));
     // loop through each hour and check with current hour
     for (var i = 0; i < schedule.hour.length; i++) {
         var scheduleHour = parseInt(schedule.hour[i]);
         var textAreaEl = $("#"+scheduleHour+"-event");
         if(scheduleHour < currentHour){
-            console.log("grey");
             textAreaEl.addClass("past");
         }
         else if (scheduleHour === currentHour) {
             textAreaEl.addClass("present");
-            console.log("red");
+
         }
         else if (scheduleHour > currentHour) {
             textAreaEl.addClass("future")
-            console.log("green");
+
         }
     }
 }
-
 
 loadEvent();
 checkDue();
